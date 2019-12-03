3361009676cede28127e248df87755a461d64c97
bvolcan <brunovolcan@gmail.com>
2019-12-03 19:50:46 -0300

    cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.use('/', require('./routes'))
 
